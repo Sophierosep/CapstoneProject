390fd7e8980d74de66e84cb22e90695f9a553876
Sophie <sophiepitcairn1999@gmail.com>
2023-06-07 21:34:23 +0100

    XCode project added

---
+++ CapstoneLittleLemon/CapstoneLittleLemon/CapstoneLittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneLittleLemonApp.swift
+//  CapstoneLittleLemon
+//
+//  Created by Sophie Pitcairn on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneLittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
